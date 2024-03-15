16c81ebab828d15d1b54d47aee78af761800dbf8
xus2019 <962617107@qq.com>
2024-03-15 14:54:06 +0800

    feat:单元测试依赖

---
+++ src/test/java/com/practice/test/DemoApplicationTests.java
@@ -0,0 +1,18 @@
+package com.practice.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author xushuai5
+ * @Description
+ * @Date 创建于 2024/3/15 14:52
+ */
+@SpringBootTest
+public class DemoApplicationTests {
+
+    @Test
+    public void testMethod(){
+        System.out.println("this is a test method");
+    }
+}
